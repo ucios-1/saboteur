acdd49ac8a2fd4dbaf64b0be2b62ab19fe94f7a1
ucios-mckinsey laptop <ucios@o2.pl>
2021-11-03 22:16:36 +0100

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Games from "./components/Games.jsx";
+import About from "./components/About.jsx";
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Games />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
