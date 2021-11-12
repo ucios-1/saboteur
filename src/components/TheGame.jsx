b7c1eefbad647af64c600b50276646f7b6c21bca
ucios-mckinsey laptop <ucios@o2.pl>
2021-11-12 22:40:31 +0100

    update the game view. Make it more phone friendly

--- src/components/TheGame.jsx
+++ src/components/TheGame.jsx
@@ -1,47 +1,63 @@
+import {useEffect, useState} from "react";
 import Navbar from "./Navbar";
 import saboteur1 from "../images/saboteur1_cover.jpg";
 
 function TheGame() {
     const playingCards = [], playingField = [];
+    const [viewWidth, updateClasses] = useState(window.visualViewport.width);
     for (let i = 0; i < 6; i++) {
-        playingCards.push(<img src={saboteur1} className="playing-card" alt="card to play" />);
+        playingCards.push(<div draggable="true" className="field field-card playing-card"></div>);
     }
-    for (let i = 0; i < 1; i++) {
-        playingField.push(<img src={saboteur1} className="playing-field" alt="card to play" />);
+    for (let i = 0; i < 45; i++) {
+        if(i === 3 || i === 1 || i === 40 || i === 41 || i === 43 || i === 44) {
+            playingField.push(<div className="col field"></div>);
+        } else {
+            playingField.push(<div className="col field field-card"></div>);
+        }
+        
     }
+
+    function update() {
+        updateClasses(window.visualViewport.width)
+    }
+    window.addEventListener("resize", update);
 
 
     return (
         <div>
             <Navbar />
             <div className="container">
-                <div className="row">
-                    <div className="col-8">
-                        <div className="row justify-content-md-center">
-                            <div className="col-2">
-                                {playingField}
-                            </div>
-                            <div className="col-2">
-                                {playingField}
-                            </div>
-                            <div className="col-2">
-                                {playingField}
-                            </div>
-                            <div className="col-2">
-                                {playingField}
-                            </div>
-                            <div className="col-2">
-                                {playingField}
-                            </div>
-                        </div>
-                        <div className="">
-                            {playingCards}
+                <div className="row row-cols-1 row-cols-sm-3 flex-center-hor">
+                    <div className="col-sm-12 col-md-1">
+                        <div className={(viewWidth < 700) ? "" : "row" }>
+                            <i className="icon icon-active fas fa-dolly-flatbed"></i>
+                            <i className="icon icon-deactive fas fa-hammer"></i>
+                            <i className="icon icon-active far fa-lightbulb"></i>
                         </div>
                     </div>
-                    <div className="col-4">
-                        <div className="players">sdsds</div>
-                        <div className="chat-ifeld">sdsds</div>
-                        <div className="input-field">sdsds</div>
+                    <div className="col-sm-12 col-md-1">
+                        <div className={(viewWidth < 700) ? "flex-center" : "row" }>
+                            <div className="player player-active">
+                                <i className="icon fas fa-user"></i>
+                                <p>user name</p>
+                            </div>
+                            <div className="player player-deactive">
+                                <i className="icon fas fa-user"></i>
+                                <p>user name</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-6">
+                        <div className="flex-center-hor col">
+                            <div className={(viewWidth < 700) ? "board add-space-bottom" : "board" }>
+                                {playingField}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-12 col-md-1">
+                        <div className={(viewWidth < 700) ? "row fixed-bottom" : "row" }>
+                            {playingCards}
+                        </div>
                     </div>
                 </div>
             </div>
