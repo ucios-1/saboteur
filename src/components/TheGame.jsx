0bc324fa03715ff453998278efa19c3d9ce2b993
ucios-mckinsey laptop <ucios@o2.pl>
2021-11-13 23:14:03 +0100

    create sidebar with players list and chat window

--- src/components/TheGame.jsx
+++ src/components/TheGame.jsx
@@ -5,6 +5,8 @@ import saboteur1 from "../images/saboteur1_cover.jpg";
 function TheGame() {
     const playingCards = [], playingField = [];
     const [viewWidth, updateClasses] = useState(window.visualViewport.width);
+    var roomName;
+
     for (let i = 0; i < 6; i++) {
         playingCards.push(<div draggable="true" className="field field-card playing-card"></div>);
     }
@@ -36,7 +38,7 @@ function TheGame() {
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-1">
-                        <div className={(viewWidth < 700) ? "flex-center" : "row" }>
+                        <div className={(viewWidth < 700) ? "flex-center flex-wrap" : "row row-cols-sm-3" }>
                             <div className="player player-active">
                                 <i className="icon fas fa-user"></i>
                                 <p>user name</p>
@@ -58,6 +60,82 @@ function TheGame() {
                         <div className={(viewWidth < 700) ? "row fixed-bottom" : "row" }>
                             {playingCards}
                         </div>
+                    </div>
+                    <button class="btn btn-light fixed-right-leftrounded" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop">
+                        <i className="icon fas fa-user"></i>
+                    </button>
+
+                    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+                    <div class="offcanvas-header">
+                        <h5 id="offcanvasTopLabel">Offcanvas top</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ol className="list-group list-group-flush list-small">
+                            <h5>Players</h5>
+                            <li class="list-group-item d-flex justify-content-between align-items-start">An item <span class="badge bg-warning text-dark rounded-pill">14</span></li>
+                            <li class="list-group-item">A second item <span class="badge bg-warning text-dark rounded-pill">14</span></li>
+                            <li class="list-group-item">A third item <span class="badge bg-warning text-dark rounded-pill">14</span></li>
+                            <li class="list-group-item list-group-item-dark">A fourth item <span class="badge bg-warning text-dark rounded-pill">14</span></li>
+                            <li class="list-group-item">And a fifth one</li>
+                            <li class="list-group-item">An item</li>
+                            <li class="list-group-item">A second item</li>
+                            <li class="list-group-item">A third item</li>
+                            <li class="list-group-item">A fourth item</li>
+                            <li class="list-group-item">And a fifth one</li>
+                        </ol>
+                        <br />
+                        <div classNmame="container">
+                            <div className="card">
+                                <div className="card-header">{roomName} chat</div>
+                                <div className="dacd-body">
+                                    <div className="messanger">
+                                        <div className="mssg message-left">
+                                            <div className="mssg-header">
+                                                <div className="player-name">
+                                                    player
+                                                </div>
+                                                <div className="message-time">
+                                                    14:17
+                                                </div>
+                                            </div>
+                                            <div className="message-body">
+                                                Hello world!
+                                            </div>
+                                        </div>
+                                        <div className="mssg message-right">
+                                            <div className="mssg-header">
+                                                <div className="player-name">
+                                                    player
+                                                </div>
+                                                <div className="message-time">
+                                                    14:20
+                                                </div>
+                                            </div>
+                                            <div className="message-body">
+                                                Hi world!
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="card-footer">
+                                    <form>
+                                        <div className="mssg-footer">
+                                            <textarea className="mssg-text" name="mssgText"></textarea>
+                                            <div className="mssg-header">
+                                                <div className="">
+                                                    <div className="mssg-options"><i class="fas fa-bold"></i></div>
+                                                    <div className="mssg-options"><i class="fas fa-italic"></i></div>
+                                                    <div className="mssg-options"><i class="fas fa-strikethrough"></i></div>
+                                                </div>
+                                                <button className="btn btn-outline-secondary"><i class="fas fa-paper-plane"></i></button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     </div>
                 </div>
             </div>
